8a24b3ec082291f66bf8bb87c6e1d2104ad9c873
hinal010 <hinalprajapati010@example.com>
2026-06-01 15:41:03 +0530

    update the clients

--- crud.py
+++ crud.py
@@ -41,26 +41,6 @@ def create_user(
 
     return db_user
 
-# def get_user_by_email(db: Session, email: str):
-#     return db.query(User).filter(User.email == email).first()
-
-
-def create_user(db: Session, user: UserCreate):
-    hashed_password = hash_password(user.password)
-
-    db_user = User(
-        name=user.name,
-        email=user.email,
-        password=hashed_password,
-        role=user.role
-    )
-
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-
-    return db_user
-
 
 def get_users(db: Session):
     return db.query(User).all()
@@ -157,7 +137,11 @@ def update_client(
     if not client:
         return None
 
-    for key, value in data.dict().items():
+    update_data = data.model_dump(
+    exclude_unset=True
+)
+
+    for key, value in update_data.items():
 
         setattr(
             client,
--- schemas.py
+++ schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from pydantic import EmailStr
+from pydantic import EmailStr,Field
 
 
 class UserCreate(BaseModel):
@@ -54,7 +54,10 @@ class ClientCreate(BaseModel):
 
     pharmacy_name: str
     contact_person: str | None = None
-    mobile_no: str
+    mobile_no: str = Field(
+    min_length=10,
+    max_length=10
+)
 
     email: EmailStr | None = None
     lead_source: str | None = None
@@ -65,16 +68,20 @@ class ClientCreate(BaseModel):
 
 class ClientUpdate(BaseModel):
 
-    pharmacy_name: str
+    pharmacy_name: str | None = None
     contact_person: str | None = None
-    mobile_no: str
+    mobile_no: str | None = Field(
+        default=None,
+        min_length=10,
+        max_length=10
+    )
 
     email: EmailStr | None = None
     lead_source: str | None = None
     address: str | None = None
 
-    city_id: int
-    area_id: int
+    city_id: int | None = None
+    area_id: int | None = None
 
 class ClientResponse(BaseModel):
 
@@ -91,6 +98,10 @@ class ClientResponse(BaseModel):
 
     city_id: int
     area_id: int
+    
+    city_name: str
+    area_name: str
+
 
     created_by: int
 
